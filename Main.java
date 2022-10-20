97e16dbb53a2dac13a5e8a10d83777edd74b1a98
Gabriel Sanches <thebug@outlook.com.br>
2022-10-20 20:52:43 -0300

    Ajuste na rotina de inserir um item para permitir inserir e editar

--- Main.java
+++ Main.java
@@ -3,39 +3,80 @@ package ProjetoInterdisciplinar_II;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
-    static List<Veiculo> ListaVeiculos = new ArrayList<Veiculo>();
+    static List<Veiculo> ListaVeiculos = new ArrayList<>();
+    static Veiculo Veiculo;
+    static Scanner sc = new Scanner(System.in);
     static String RetonaTipoVeiculo(int IdentificadorVeiculo){
-        String TipoVeiculo = switch (IdentificadorVeiculo) {
+        return switch (IdentificadorVeiculo) {
             case 1 -> "Moto";
             case 2 -> "Carro";
             case 3 -> "Caminhao";
             default -> throw new IllegalStateException("Unexpected value: " + IdentificadorVeiculo);
         };
-        return TipoVeiculo;
     }
 
-    static void ListaVeiculos(List<Veiculo> ListaVeiculos){
+    static void ListaVeiculos(List<Veiculo> ListaVeiculos) {
         int Contador = 0;
-        for(Veiculo Veiculo : ListaVeiculos)
-        {
+        for (Veiculo Veiculo : ListaVeiculos) {
             System.out.printf("%d - %s%n", Contador, Veiculo.RetornaDadosVeiculo());
             Contador++;
         }
     }
 
+    static void CadastraAtualizaVeiculo(String mode, Veiculo Veiculo, int ItemAtualizar){
+        int TipoVeiculo = 0;
+        if((Objects.equals(mode, "INS")) || ((Objects.equals(mode, "UPD")) && (ItemAtualizar == 1))) {
+            System.out.println("Digite a marca do veiculo: ");
+            Veiculo.setMarca(sc.next());
+        }
+        if((Objects.equals(mode, "INS")) || ((Objects.equals(mode, "UPD")) && (ItemAtualizar == 2))) {
+            System.out.println("Digite o Modelo do veiculo: ");
+            Veiculo.setModelo(sc.next());
+        }
+        if((Objects.equals(mode, "INS")) || ((Objects.equals(mode, "UPD")) && (ItemAtualizar == 3))) {
+            System.out.println("Digite o estado do veiculo: ");
+            Veiculo.setEstado(sc.next());
+        }
+        if((Objects.equals(mode, "INS")) || ((Objects.equals(mode, "UPD")) && (ItemAtualizar == 4))) {
+            System.out.println("Digite a cor do veiculo: ");
+            Veiculo.setCor(sc.next());
+        }
+        if((Objects.equals(mode, "INS")) || ((Objects.equals(mode, "UPD")) && (ItemAtualizar == 5))) {
+            System.out.println("Digite o numero referente ao tipo do veiculo (1)-MOTO, (2)-CARRO e (3)-CAMINHÃO: ");
+            while ((TipoVeiculo != 1) && (TipoVeiculo != 2) && (TipoVeiculo != 3)) {
+                TipoVeiculo = sc.nextInt();
+
+            }
+
+            Veiculo.setTipoVeiculo(RetonaTipoVeiculo(TipoVeiculo));
+        }
+        if((Objects.equals(mode, "INS")) || ((Objects.equals(mode, "UPD")) && (ItemAtualizar == 6))) {
+            System.out.println("Digite o valor do veiculo: ");
+            Veiculo.setValor(sc.nextDouble());
+        }
+        if((Objects.equals(mode, "INS")) || ((Objects.equals(mode, "UPD")) && (ItemAtualizar == 7))) {
+            System.out.println("Digite a kilometragem do veiculo: ");
+            Veiculo.setKilometragem(sc.nextDouble());
+        }
+        if((Objects.equals(mode, "INS")) || ((Objects.equals(mode, "UPD")) && (ItemAtualizar == 8))) {
+            System.out.println("Digite a quantidade de veiculos disponiveis: ");
+            Veiculo.setQuantidadeDisponivel(sc.nextInt());
+        }
+        if(Objects.equals(mode, "INS")) {
+            ListaVeiculos.add(Veiculo);
+        }
+    }
+
     public static void main(String[] args){
         //Declaração de variaveis
-        int MenuEscolha, TipoVeiculo;
-        Veiculo Veiculo;
-        /*List<Veiculo> ListaVeiculos = new ArrayList<Veiculo>();*/
-        Scanner sc = new Scanner(System.in);
+        int MenuEscolha;
 
         //Inicialização de variaveis
         MenuEscolha = 1;
-        TipoVeiculo = 0;
 
         //Mensagem com as opções do menu
         System.out.println("""
@@ -54,10 +95,9 @@ public class Main {
 
                 case 1: /*Lista de veiculos cadastrados*/
                     /*System.out.println("Opção 1");*/
-
-                    if (ListaVeiculos.isEmpty()){
+                    if (ListaVeiculos.isEmpty()) {
                         System.out.println("Ainda não foi cadastrado nenhum item para venda!");
-                    }else {
+                    } else {
                         ListaVeiculos(ListaVeiculos);
                     }
                     break;
@@ -65,42 +105,24 @@ public class Main {
                 case 2: /*Cadastro de novos veiculos*/
 
                     Veiculo = new Veiculo();
+                    CadastraAtualizaVeiculo("INS", Veiculo, 0);
+                    break;
 
-                    System.out.println("Digite a marca do veiculo: ");
-                    Veiculo.setMarca(sc.next());
+                case 3:/*Editar um item*/
 
-                    System.out.println("Digite o Modelo do veiculo: ");
-                    Veiculo.setModelo(sc.next());
-
-                    System.out.println("Digite o estado do veiculo: ");
-                    Veiculo.setEstado(sc.next());
-
-                    System.out.println("Digite a cor do veiculo: ");
-                    Veiculo.setCor(sc.next());
-
-                    System.out.println("Digite o numero referente ao tipo do veiculo (1)-MOTO, (2)-CARRO e (3)-CAMINHÃO: "); /*Colocar enum*/
-                    while((TipoVeiculo != 1) && (TipoVeiculo != 2) && (TipoVeiculo != 3) ) {
-                        TipoVeiculo = sc.nextInt();
-
+                    if (ListaVeiculos.isEmpty()) {
+                        System.out.println("Ainda não foi cadastrado nenhum item para venda!");
+                    } else {
+                        ListaVeiculos(ListaVeiculos);
+                        System.out.println("Digite o id do item que deseja editar: ");
+                        CadastraAtualizaVeiculo("UPD", ListaVeiculos.get(sc.nextInt()), 1);
                     }
-
-                    Veiculo.setTipoVeiculo(RetonaTipoVeiculo(TipoVeiculo));
-
-                    System.out.println("Digite o valor do veiculo: ");
-                    Veiculo.setValor(sc.nextDouble());
-
-                    System.out.println("Digite a kilometragem do veiculo: ");
-                    Veiculo.setKilometragem(sc.nextDouble());
-
-                    System.out.println("Digite a quantidade de veiculos disponiveis: ");
-                    Veiculo.setQuantidadeDisponivel(sc.nextInt());
-
-                    ListaVeiculos.add(Veiculo);
                     break;
 
                 case 0:
                     MenuEscolha = 0;
                     break;
+
                 default:
                     System.out.println("Opção invalida tente novamente!");
             }
