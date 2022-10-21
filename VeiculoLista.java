17b1695671cd2cec58bf3a378ffcdfc9b86a9930
Gabriel Sanches <thebug@outlook.com.br>
2022-10-21 14:36:57 -0300

    Adição de comentario

--- VeiculoLista.java
+++ VeiculoLista.java
@@ -2,12 +2,12 @@ package ProjetoInterdisciplinar_II;
 
 import java.util.List;
 public class VeiculoLista {
-
+    /*Lista todos os veiculos cadastrados*/
     static void Lista(List<Veiculo> ListaVeiculos){
         int Contador = 0;
-       for (Veiculo Veiculo : ListaVeiculos) {
+        for (Veiculo Veiculo : ListaVeiculos) {
            System.out.printf("%d - %s%n", Contador, Veiculo.RetornaDadosVeiculo());
-            Contador++;
+           Contador++;
         }
     }
 
