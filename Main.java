2637b40a79ec6e9b527f69787de3a6e6346184cd
Gabriel Sanches <thebug@outlook.com.br>
2022-10-21 14:57:00 -0300

    Alteração para a chamada da nova classe de cadastrar ou inserir um item

--- Main.java
+++ Main.java
@@ -3,69 +3,17 @@ package ProjetoInterdisciplinar_II;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
     static List<Veiculo> ListaVeiculos = new ArrayList<>();
-    static Veiculo Veiculo;
-    static Scanner sc = new Scanner(System.in);
-    static String RetonaTipoVeiculo(int IdentificadorVeiculo){
-        return switch (IdentificadorVeiculo) {
-            case 1 -> "Moto";
-            case 2 -> "Carro";
-            case 3 -> "Caminhao";
-            default -> throw new IllegalStateException("Unexpected value: " + IdentificadorVeiculo);
-        };
-    }
-
-    static void CadastraAtualizaVeiculo(String mode, Veiculo Veiculo, int ItemAtualizar){
-        int TipoVeiculo = 0;
-        if((Objects.equals(mode, "INS")) || ((Objects.equals(mode, "UPD")) && (ItemAtualizar == 1 || ItemAtualizar == 0))) {
-            System.out.println("Digite a marca do veiculo: ");
-            Veiculo.setMarca(sc.next());
-        }
-        if((Objects.equals(mode, "INS")) || ((Objects.equals(mode, "UPD")) && (ItemAtualizar == 2 || ItemAtualizar == 0))) {
-            System.out.println("Digite o Modelo do veiculo: ");
-            Veiculo.setModelo(sc.next());
-        }
-        if((Objects.equals(mode, "INS")) || ((Objects.equals(mode, "UPD")) && (ItemAtualizar == 3 || ItemAtualizar == 0))) {
-            System.out.println("Digite o estado do veiculo: ");
-            Veiculo.setEstado(sc.next());
-        }
-        if((Objects.equals(mode, "INS")) || ((Objects.equals(mode, "UPD")) && (ItemAtualizar == 4 || ItemAtualizar == 0))) {
-            System.out.println("Digite a cor do veiculo: ");
-            Veiculo.setCor(sc.next());
-        }
-        if((Objects.equals(mode, "INS")) || ((Objects.equals(mode, "UPD")) && (ItemAtualizar == 5 || ItemAtualizar == 0))) {
-            System.out.println("Digite o numero referente ao tipo do veiculo (1)-MOTO, (2)-CARRO e (3)-CAMINHÃO: ");
-            while ((TipoVeiculo != 1) && (TipoVeiculo != 2) && (TipoVeiculo != 3)) {
-                TipoVeiculo = sc.nextInt();
-
-            }
-
-            Veiculo.setTipoVeiculo(RetonaTipoVeiculo(TipoVeiculo));
-        }
-        if((Objects.equals(mode, "INS")) || ((Objects.equals(mode, "UPD")) && (ItemAtualizar == 6 || ItemAtualizar == 0))) {
-            System.out.println("Digite o valor do veiculo: ");
-            Veiculo.setValor(sc.nextDouble());
-        }
-        if((Objects.equals(mode, "INS")) || ((Objects.equals(mode, "UPD")) && (ItemAtualizar == 7 || ItemAtualizar == 0))) {
-            System.out.println("Digite a kilometragem do veiculo: ");
-            Veiculo.setKilometragem(sc.nextDouble());
-        }
-        if((Objects.equals(mode, "INS")) || ((Objects.equals(mode, "UPD")) && (ItemAtualizar == 8 || ItemAtualizar == 0))) {
-            System.out.println("Digite a quantidade de veiculos disponiveis: ");
-            Veiculo.setQuantidadeDisponivel(sc.nextInt());
-        }
-        if(Objects.equals(mode, "INS")) {
-            ListaVeiculos.add(Veiculo);
-        }
-    }
 
     public static void main(String[] args){
         //Declaração de variaveis
+        Scanner sc = new Scanner(System.in);
         int MenuEscolha;
+        Veiculo Veiculo;
+        List<Veiculo> ListaVeiculos = new ArrayList<>();
 
         //Inicialização de variaveis
         MenuEscolha = 1;
@@ -97,7 +45,7 @@ public class Main {
                 case 2: /*Cadastro de novos veiculos*/
 
                     Veiculo = new Veiculo();
-                    CadastraAtualizaVeiculo("INS", Veiculo, 0);
+                    Veiculo_CadastroAtualizacao.CadastraAtualizaVeiculo("INS", Veiculo, 0);
                     break;
 
                 case 3:/*Editar um item*/
@@ -107,7 +55,7 @@ public class Main {
                     } else {
                         VeiculoLista.Lista(ListaVeiculos);
                         System.out.println("Digite o id do item que deseja editar: ");
-                        CadastraAtualizaVeiculo("UPD", ListaVeiculos.get(sc.nextInt()), 1);
+                        Veiculo_CadastroAtualizacao.CadastraAtualizaVeiculo("UPD", ListaVeiculos.get(sc.nextInt()), 1);
                     }
                     break;
 
