b2fd47ce65be865be42e5e47db5dc178d05600ce
Gabriel <thebug@outlook.com.br>
2022-10-25 09:45:19 -0300

    Adicção da mensagem com as escolhas do item a atualizar e adição de comentarios

--- Main.java
+++ Main.java
@@ -8,20 +8,35 @@ import java.util.Scanner;
 public class Main {
     static List<Veiculo> ListaVeiculos = new ArrayList<>();
 
-    public static void mensagemOpcao() {
-        System.out.println("""
-                Opções disponiveis.
-                 1 - Ver lista de veiculos cadastrados
-                 2 - Cadastrar novo veiculo\s
-                 3 - Editar Registro
-                 4 - Excluir Resgitro
-                 0 - Sair""");
+    public static void mensagemOpcao(int OpcaoMensagem) {
+        switch (OpcaoMensagem) {
+            case 1 -> System.out.println("""
+                    Opções disponiveis.
+                     1 - Ver lista de veiculos cadastrados
+                     2 - Cadastrar novo veiculo\s
+                     3 - Editar Registro
+                     4 - Excluir Resgitro
+                     0 - Sair""");
+            case 2 -> System.out.println("""
+                    Digite o numero do campo que deseja atualizar.
+                     1 - Marca
+                     2 - Modelo\s
+                     3 - Estado
+                     4 - Cor
+                     5 - Tipo do veiculo
+                     6 - Valor
+                     7 - Kilometragem
+                     8 - Quantidade disponivel
+                     0 - Todos os campos
+                     """);
+        }
+
     }
 
     public static void main(String[] args) {
         // Declaração de variaveis
         Scanner sc = new Scanner(System.in);
-        int MenuEscolha, contador;
+        int MenuEscolha, contador, ItemAtualizar, indexDoVeliculo;
         Veiculo Veiculo;
 
         // Inicialização de variaveis
@@ -32,16 +47,18 @@ public class Main {
 
             if (contador == 0) {
                 // Mensagem com as opções do menu
-                mensagemOpcao();
+                mensagemOpcao(1);
                 contador = 3;
-            }
+            } /* Mostra as opções disponiveis a cada 3 tentativas do usuario */
             contador -= 1;
+
             System.out.println("Digite o numero correspondente a opção que deseja acessar: ");
             MenuEscolha = sc.nextInt();
 
             switch (MenuEscolha) {
 
                 case 1: /* Lista de veiculos cadastrados */
+
                     /* System.out.println("Opção 1"); */
                     if (ListaVeiculos.isEmpty()) {
                         System.out.println("Ainda não foi cadastrado nenhum item para venda!");
@@ -63,11 +80,15 @@ public class Main {
                     } else {
                         VeiculoLista.Lista(ListaVeiculos);
                         System.out.println("Digite o id do item que deseja editar: ");
-                        Veiculo_CadastroAtualizacao.CadastraAtualizaVeiculo("UPD", ListaVeiculos.get(sc.nextInt()), 1);
+                        indexDoVeliculo = sc.nextInt();
+
+                        mensagemOpcao(2);
+                        ItemAtualizar = sc.nextInt();
+                        Veiculo_CadastroAtualizacao.CadastraAtualizaVeiculo("UPD", ListaVeiculos.get(indexDoVeliculo), ItemAtualizar);
                     }
                     break;
 
-                case 4:/* Editar um item */
+                case 4:/* Exluir um item */
 
                     if (ListaVeiculos.isEmpty()) {
                         System.out.println("Ainda não foi cadastrado nenhum item para venda!");
